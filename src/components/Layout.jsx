20cf039b6d127301e89aea5d636b2e37f71c983b
zaltsani <zaltsani1@gmail.com>
2024-09-02 00:22:21 +0700

    update module

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
-// import { Navbar } from "react-bootstrap";
 
 function Layout(props) {
     return (
         <div>
             <NavigationBar />
-            {/* <Navbar></Navbar> */}
             <div>{ props.children }</div>
         </div>
     );
