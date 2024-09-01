6c1201f0817d97d844f11f9e54c0d1f0b1ca763d
zaltsani <zaltsani1@gmail.com>
2024-09-02 00:20:41 +0700

    main

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import { Button } from 'react-bootstrap'
 
 function Home() {
-  return (
-    <Layout>
-        <div>home</div>
-        <Button>Test</Button>
-    </Layout>
-  )
+    const [matchesData, setMatchesData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/55/282.json")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Response Not OK")
+                }
+                return response.json();
+            })
+            .then(data => {
+                setMatchesData(data);
+            })
+    }, [])
+    console.log(matchesData)
+    return (
+        <Layout>
+            <div>home</div>
+            <Button>Test</Button>
+        </Layout>
+    )
 }
 
 export default Home
