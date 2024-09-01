987f9785c7bd6376485a389ba954e25ab662e9be
zaltsani <zaltsani1@gmail.com>
2024-09-01 21:57:30 +0700

    add component and styles files

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar, Container } from "react-bootstrap";
+import '../styles/Navbar.css'
+
+
+function NavigationBar() {
+  return (
+    <Navbar style={{
+            background: "#D9D9D9",
+            height: "81px"
+          }}>
+      <Container>
+        <Navbar.Brand className='brand' href='/'>
+          StatsFlood
+        </Navbar.Brand>
+
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
